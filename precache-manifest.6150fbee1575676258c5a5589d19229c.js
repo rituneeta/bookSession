100401b0afd5b2b6703ac5fcc8d90dad87b83455
rituneeta <rituneeta@gmail.com>
2020-10-22 18:29:09 +0530

    Updates

---
+++ precache-manifest.6150fbee1575676258c5a5589d19229c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f70b946afa3423687cae7683858f2b8",
+    "url": "/bookSession/index.html"
+  },
+  {
+    "revision": "66ca8a4900706d941778",
+    "url": "/bookSession/static/css/2.a7288257.chunk.css"
+  },
+  {
+    "revision": "7f7633a5729cb9b59021",
+    "url": "/bookSession/static/css/main.70408bd9.chunk.css"
+  },
+  {
+    "revision": "66ca8a4900706d941778",
+    "url": "/bookSession/static/js/2.e93b3b8f.chunk.js"
+  },
+  {
+    "revision": "179fe3fc0e78b5c2b8e2be1fa47bcaa6",
+    "url": "/bookSession/static/js/2.e93b3b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7633a5729cb9b59021",
+    "url": "/bookSession/static/js/main.89720548.chunk.js"
+  },
+  {
+    "revision": "3e303dad089c36f000ff",
+    "url": "/bookSession/static/js/runtime-main.6d4763de.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/bookSession/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/bookSession/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/bookSession/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/bookSession/static/media/notification.c0d3c94c.eot"
+  }
+]);
