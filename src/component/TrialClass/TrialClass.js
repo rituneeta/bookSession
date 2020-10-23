8c6f9df4cacbf1d713559bea6cdb7661d16a4fba
rituneeta <rituneeta@gmail.com>
2020-10-23 12:30:30 +0530

    final submit

--- src/component/TrialClass/TrialClass.js
+++ src/component/TrialClass/TrialClass.js
@@ -13,6 +13,7 @@ const TrialClass = () => {
   const [dateSelect, setDateSelect] = useState(null);
   const [validated, setValidated] = useState(false);
   const formRef = useRef(null);
+  const [showError, setShowError] = useState();
 
   useEffect(() => {
     axios
@@ -59,15 +60,6 @@ const TrialClass = () => {
     } else {
       delete newErrors["email"];
     }
-    if (newErrors.hasOwnProperty("parentName")) {
-      delete newErrors["parentName"];
-    }
-    if (newErrors.hasOwnProperty("childName")) {
-      delete newErrors["childName"];
-    }
-    if (newErrors.hasOwnProperty("timeSlot")) {
-      delete newErrors["timeSlot"];
-    }
     setErrors(newErrors);
     if (!Object.keys(newErrors).length) {
       axios
@@ -79,144 +71,126 @@ const TrialClass = () => {
           setValidated(false);
         })
         .catch((error) => {
-          NotificationManager.error(
-            "please enter" + error.response.data.message,
-            "",
-            2000
-          );
-          let errormsg = error.response.data.message.split(",");
-          let newErrors = { ...errors };
-          for (let key of errormsg) {
-            newErrors[key] = key;
-          }
-          setErrors(newErrors);
+          setShowError(error.response.data.message);
         });
     }
   };
   return (
     <>
       {courseData ? (
-        <Form onSubmit={submitForm} ref={formRef} validated={validated}>
-          <Form.Group controlId="pname">
-            <Form.Label>Parent's Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter parent name" />
-            {errors.hasOwnProperty("parentName") && (
-              <p
-                style={{ color: "red" }}
-              >{`Please Enter the ${errors.parentName}`}</p>
-            )}
-          </Form.Group>
-          <Form.Group controlId="pnumber">
-            <Form.Label>Parent's Contact Number</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter parent Contact number"
-            />{" "}
-            {errors.hasOwnProperty("phoneNumber") && (
-              <p style={{ color: "red" }}>{errors.phoneNumber}</p>
-            )}
-          </Form.Group>
-
-          <Form.Group controlId="pemail">
-            <Form.Label>Parent's Email ID</Form.Label>
-            <Form.Control type="email" placeholder="Enter parent email" />
-            {errors.hasOwnProperty("email") && (
-              <p style={{ color: "red" }}>{errors.email}</p>
-            )}
-          </Form.Group>
-          <Form.Group controlId="cname">
-            <Form.Label>Child's Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter child name" />
-            {errors.hasOwnProperty("childName") && (
-              <p
-                style={{ color: "red" }}
-              >{`Please Enter the ${errors.childName}`}</p>
-            )}
-          </Form.Group>
-          <Form.Group controlId="cage">
-            <Form.Label>Child's Age</Form.Label>
-            <Form.Control type="number" placeholder="Enter child age" />
-          </Form.Group>
-          <Form.Group controlId="coursename">
-            <Form.Label>Course Name</Form.Label>
-            <Form.Control
-              as="select"
-              type="text"
-              onChange={(e) => {
-                let data = e.target.value;
-                setCourseName(data);
-              }}
-            >
-              <option value="">Select the course</option>
-              {courseData.map((course) => {
-                return (
-                  <option value={course.course_name}>
-                    {course.course_name}
-                  </option>
-                );
-              })}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="classdate">
-            <Form.Label>Suitable Date for a trial class</Form.Label>
-            <Form.Control
-              type="date"
-              min={moment(new Date()).format("YYYY-MM-DD ")}
-              max={moment(new Date().setDate(new Date().getDate() + 7)).format(
-                "YYYY-MM-DD"
+        <>
+          {showError && (
+            <div class="alert alert-danger" role="alert">
+              {showError}
+            </div>
+          )}
+          <Form onSubmit={submitForm} ref={formRef} validated={validated}>
+            <Form.Group controlId="pname">
+              <Form.Label>Parent's Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter parent name" />
+            </Form.Group>
+            <Form.Group controlId="pnumber">
+              <Form.Label>Parent's Contact Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter parent Contact number"
+              />{" "}
+              {errors.hasOwnProperty("phoneNumber") && (
+                <p style={{ color: "red" }}>{errors.phoneNumber}</p>
               )}
-              onChange={(e) => {
-                let date = e.target.value;
-                setDateSelect(moment(date));
-              }}
-              onKeyDown={(e) => e.preventDefault()}
-            />
-          </Form.Group>
-          <Form.Group controlId="timeslot">
-            <Form.Label>Suitable Time Slot for the trial class</Form.Label>
-            <Form.Control
-              as="select"
-              type="time"
-              placeholder="Enter the time slot"
-            >
-              <option value="">Select the time slot</option>
-              {courseName &&
-                dateSelect &&
-                courseData
-                  .filter((course) => course.course_name === courseName)[0]
-                  .slots.filter((slot) => {
-                    if (slot.time.date() === dateSelect.date()) {
-                      let slot_time = slot.time;
-                      let finalTime = slot_time.diff(moment(), "hours");
-                      if (finalTime >= 4) {
-                        return true;
+            </Form.Group>
+
+            <Form.Group controlId="pemail">
+              <Form.Label>Parent's Email ID</Form.Label>
+              <Form.Control type="email" placeholder="Enter parent email" />
+              {errors.hasOwnProperty("email") && (
+                <p style={{ color: "red" }}>{errors.email}</p>
+              )}
+            </Form.Group>
+            <Form.Group controlId="cname">
+              <Form.Label>Child's Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter child name" />
+            </Form.Group>
+            <Form.Group controlId="cage">
+              <Form.Label>Child's Age</Form.Label>
+              <Form.Control type="number" placeholder="Enter child age" />
+            </Form.Group>
+            <Form.Group controlId="coursename">
+              <Form.Label>Course Name</Form.Label>
+              <Form.Control
+                as="select"
+                type="text"
+                onChange={(e) => {
+                  let data = e.target.value;
+                  setCourseName(data);
+                }}
+              >
+                <option value="">Select the course</option>
+                {courseData.map((course) => {
+                  return (
+                    <option value={course.course_name}>
+                      {course.course_name}
+                    </option>
+                  );
+                })}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="classdate">
+              <Form.Label>Suitable Date for a trial class</Form.Label>
+              <Form.Control
+                type="date"
+                min={moment(new Date()).format("YYYY-MM-DD ")}
+                max={moment(
+                  new Date().setDate(new Date().getDate() + 7)
+                ).format("YYYY-MM-DD")}
+                onChange={(e) => {
+                  let date = e.target.value;
+                  setDateSelect(moment(date));
+                }}
+                onKeyDown={(e) => e.preventDefault()}
+              />
+            </Form.Group>
+            <Form.Group controlId="timeslot">
+              <Form.Label>Suitable Time Slot for the trial class</Form.Label>
+              <Form.Control
+                as="select"
+                type="time"
+                placeholder="Enter the time slot"
+              >
+                <option value="">Select the time slot</option>
+                {courseName &&
+                  dateSelect &&
+                  courseData
+                    .filter((course) => course.course_name === courseName)[0]
+                    .slots.filter((slot) => {
+                      if (slot.time.date() === dateSelect.date()) {
+                        let slot_time = slot.time;
+                        let finalTime = slot_time.diff(moment(), "hours");
+                        if (finalTime >= 4) {
+                          return true;
+                        } else {
+                          return false;
+                        }
                       } else {
                         return false;
                       }
-                    } else {
-                      return false;
-                    }
-                  })
-                  .map((timeSlot) => {
-                    let slot_time = moment(timeSlot.time);
-                    return (
-                      <option value={timeSlot.slot}>
-                        {timeSlot.time.format("LT")} --{" "}
-                        {slot_time.add("1", "hours").format("LT")}
-                      </option>
-                    );
-                  })}
-            </Form.Control>
-            {errors.hasOwnProperty("timeSlot") && (
-              <p
-                style={{ color: "red" }}
-              >{`Please Enter the ${errors.timeSlot}`}</p>
-            )}
-          </Form.Group>
-          <Button variant="primary" type="submit" style={{ width: "100%" }}>
-            Submit
-          </Button>
-        </Form>
+                    })
+                    .map((timeSlot) => {
+                      let slot_time = moment(timeSlot.time);
+                      return (
+                        <option value={timeSlot.slot}>
+                          {timeSlot.time.format("LT")} --{" "}
+                          {slot_time.add("1", "hours").format("LT")}
+                        </option>
+                      );
+                    })}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit" style={{ width: "100%" }}>
+              Submit
+            </Button>
+          </Form>
+        </>
       ) : (
         <h1>Loading....</h1>
       )}
